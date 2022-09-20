fd9ecf706825cc3bdccfb434ba56337eab0267e3
sina <sinarahimzade.edu@gmail.com>
2022-09-20 19:37:44 +0430

    CONF: adding config urls and driver path

---
+++ execution_engine/config.py
@@ -0,0 +1,8 @@
+TSE_MARKET_WATCH_PLUS = (
+    "http://www.tsetmc.com/tsev2/data/MarketWatchPlus.aspx?h=0&r=10392282875"
+)
+TSE_ISNT_INFO_URL = "http://www.tsetmc.com/tsev2/data/instinfofast.aspx?i={}&c=0&e=1"
+
+CHROME_EXECUTABLE_PATH = (
+    "/Users/sina/tmp/trade_exec/exectrade/execution_engine/chromedriver"
+)
