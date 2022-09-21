5dc1ed61732cbad1d311c2513b935d58b58aab6f
sina <sinarahimzade.edu@gmail.com>
2022-09-21 16:02:29 +0430

    CLEAN: removing headers

--- execution_engine/mofid.py
+++ execution_engine/mofid.py
@@ -14,16 +14,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 import execution_engine.config as config
 
-HEADERS = {
-    "Accept": "application/json, text/plain, */*",
-    "Authorization": "{token}",
-    "Referer": "https://mobile.emofid.com/",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-    "(KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36",
-    "sec-ch-ua": '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
-    "sec-ch-ua-mobile": "?0",
-}
-
 
 class Authentication(ABC):
     def __init__(self):
