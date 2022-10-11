7273a2d937bf8f68541774bba9ba0c352b47a1ba
sina <sinarahimzade.edu@gmail.com>
2022-10-12 00:24:28 +0330

    TEMP: temporary db writer

--- execution_engine/influx.py
+++ execution_engine/influx.py
@@ -1,26 +1,41 @@
 from datetime import datetime
-
+from time import sleep
 from influxdb_client import InfluxDBClient, Point, WritePrecision
-from influxdb_client.client.write_api import SYNCHRONOUS
+from influxdb_client.client.write_api import SYNCHRONOUS, ASYNCHRONOUS
+from execution_engine.trackers.tse import TseMultipleTickerTracker
 
 
 # You can generate an API token from the "API Tokens Tab" in the UI
 token = "7VqIFiNN5RmJX9_CmaNiCo5jKTGkC0dchJIv0MSiJi7jwTN3Ziw89T0i2JmtX9uvBl91Ehy4i0f0oUk9k8GZPQ=="
 org = "delta"
-bucket = "stocks"
-
+bucket = "test"
 
 
 class DataBase:
-    def __init__(self, token: str, org: str, bucket: str):
-        self.token = token
-        self.org = org
-        self.bucket = bucket
-        self.client = InfluxDBClient(url="http://localhost:8086", token=token, org=org)
+    token = "7VqIFiNN5RmJX9_CmaNiCo5jKTGkC0dchJIv0MSiJi7jwTN3Ziw89T0i2JmtX9uvBl91Ehy4i0f0oUk9k8GZPQ=="
+    org = "delta"
+    bucket = "test"
+    client = InfluxDBClient(url="http://localhost:8086", token=token, org=org)
+    write_api = client.write_api(write_options=ASYNCHRONOUS)
 
-    def update(self,symbol: str, point: str, value: str):
-        write_api = self.client.write_api(write_options=SYNCHRONOUS)
-        p = Point(point).tag("symbol", symbol).field("value", value)
-        write_api.write(bucket=self.bucket, record=p)
-        write_api.close()
-        
+
+def updater(tracker):
+    while True:
+        try:
+            data = tracker.get_info()
+        except Exception as e:
+            print(e)
+            continue
+        print("download")
+        for info in data:
+            try:
+                DataBase.write_api.write(
+                    bucket=DataBase.bucket, record=info.to_line_protocol()
+                )
+                print("write")
+            except:
+                print(info.to_line_protocol())
+        print("_________________________________")
+
+
+updater(TseMultipleTickerTracker())
